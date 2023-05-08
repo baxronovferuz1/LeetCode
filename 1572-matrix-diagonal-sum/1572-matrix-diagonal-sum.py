9b0f1b4593d1634a65a31ba31ae8ad52550a3fc3
Feruz <109802906+baxronovferuz1@users.noreply.github.com>
2023-05-08 17:41:13 +0500

    Time: 135 ms (5.85%), Space: 16.6 MB (11.57%) - LeetHub

---
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
@@ -0,0 +1,24 @@
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        s=0
+        for i in range(len(mat)):
+            for j in range(len(mat)):
+                # i=j for Principal diagonal & i+j=matrix.length -1 for Secondary diagonal
+                if i==j or i+j==len(mat)-1:
+                    s+=mat[i][j]
+        return s
+    # def diagonalSum(self, mat: List[List[int]]) -> int:
+    #     x=mat[0]
+    #     y=mat[1]
+    #     z=mat[2]
+    #     total=x[0]+x[-1]+y[1]+z[0]+z[-1]
+    #     return total
+#         top_under=[]
+#         middle=[]
+#         for a in mat:
+#             if a[0] and a[-1]:
+#                 some=top_under.append(a)
+#             else:
+#                 wip=middle.append(a)
+            
+        
