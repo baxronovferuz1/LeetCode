49c2e16d0b41839302098e60f38f87b68a141c1d
Feruz <109802906+baxronovferuz1@users.noreply.github.com>
2023-05-27 15:20:10 +0500

    Time: 539 ms (87.58%), Space: 31.8 MB (21.59%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -1,8 +1,13 @@
-class Solution(object):
-    def containsDuplicate(self, nums):
-        hset = set()
-        for idx in nums:
-            if idx in hset:
-                return True
-            else:
-                hset.add(idx)
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(set(nums)) != len(nums)
+                
+                
+# class Solution(object):
+#     def containsDuplicate(self, nums):
+#         hset = set()
+#         for idx in nums:
+#             if idx in hset:
+#                 return True
+#             else:
+#                 hset.add(idx)
