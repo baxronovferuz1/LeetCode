cd6db6af9a9a3abc6322b539066de1e5bfa7b2d0
Feruz <109802906+baxronovferuz1@users.noreply.github.com>
2023-06-19 16:34:09 +0500

    Time: 50 ms (56.79%), Space: 16.3 MB (43.47%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        for i in t:
+            if s.count(i)!=t.count(i):
+                return i
+        
+        
+        
+        
+        
+        
+    	# s, t = sorted(s), sorted(t)
+    	# for i in range(len(t)):
+    	# if t[i] not in s[i]:
+    	# return t[i]
