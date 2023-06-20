3ca19013a68aa161f7f80755c50a11df1480c3d1
Feruz <109802906+baxronovferuz1@users.noreply.github.com>
2023-06-20 21:22:42 +0500

    Time: 205 ms (17.67%), Space: 17.1 MB (6.44%) - LeetHub

--- 459-repeated-substring-pattern/459-repeated-substring-pattern.py
+++ 459-repeated-substring-pattern/459-repeated-substring-pattern.py
@@ -1,4 +1,6 @@
 class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
-        return s in s[1:] + s[:-1] 
+        # return s in s[1:] + s[:-1] 
+        
+        return True if re.search(r'^(.+)\1+$', s) else False
         
