0fef5b4d2431c3e275312e5c87238fe9b2926495
Feruz <109802906+baxronovferuz1@users.noreply.github.com>
2023-06-20 20:31:04 +0500

    Time: 80 ms (9.43%), Space: 17.9 MB (16.38%) - LeetHub

--- 414-third-maximum-number/414-third-maximum-number.py
+++ 414-third-maximum-number/414-third-maximum-number.py
@@ -3,12 +3,14 @@ class Solution:
         if len(set(nums))<3:
             return max(nums)
         return sorted((set(nums)),reverse=True)[2]
-#         if len(nums)>=3:
-#             nums=list(sorted(set(nums)))
-#             num=nums[-3]
-#             return num
-#         else:
-#             return max(nums)
+        
+        
+        # if len(nums)>=3:
+        #     nums=sorted(set(nums),reverse=True)
+        #     # num=nums[2]
+        #     return nums[2]
+        # else:
+        #     return max(nums)
         
         
         
