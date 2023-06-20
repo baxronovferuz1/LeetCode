cfaca778442d36c1b6b66589800e2a29eec89b2b
Feruz <109802906+baxronovferuz1@users.noreply.github.com>
2023-06-20 21:23:28 +0500

    Time: 216 ms (16.43%), Space: 17.3 MB (6.44%) - LeetHub

--- 459-repeated-substring-pattern/459-repeated-substring-pattern.py
+++ 459-repeated-substring-pattern/459-repeated-substring-pattern.py
@@ -2,5 +2,8 @@ class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
         # return s in s[1:] + s[:-1] 
         
-        return True if re.search(r'^(.+)\1+$', s) else False
+        if re.search(r'^(.+)\1+$', s) :
+            return True
+        else:
+            False
         
