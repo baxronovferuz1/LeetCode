1fd3b3b4726bbb0a17a87dbeb51cb89d32b78007
Feruz <109802906+baxronovferuz1@users.noreply.github.com>
2023-07-01 17:05:18 +0500

    Time: 287 ms (7.29%), Space: 16.5 MB (74.10%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.py
+++ 496-next-greater-element-i/496-next-greater-element-i.py
@@ -1,6 +1,5 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        # final_list = []
         l = []
         for i in nums1:
             for j in range(len(nums2)):
